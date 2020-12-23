7eeb85612d80485931a9b4e1b35c9fff843d9385
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-12-23 14:31:27 +0100

    initial

---
+++ index.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+/** @typedef { import('./types.d').ShipStorage } ShipStorage */
+/** @typedef { import('./types.d').StorageItem } StorageItem */
+
+/** @type ShipStorage */
+const storage = {
+  max: undefined,
+  items: [],
+};
+
+Object.defineProperty(storage, "max", { writable: true, value: 5000 });
+
+let currentStorage = undefined;
+
+function storageUsed() {
+  if (currentStorage) {
+    return currentStorage;
+  }
+  currentStorage = 0;
+  for (let i = 0; i < storage.items.length; i++) {
+    currentStorage += storage.items[i].weight;
+  }
+  return currentStorage;
+}
+
+/**
+ * @param {StorageItem} item
+ */
+function add(item) {
+  if (storage.max - item.weight >= storageUsed()) {
+    storage.items.push(item);
+    currentStorage += item.weight;
+  }
+}
